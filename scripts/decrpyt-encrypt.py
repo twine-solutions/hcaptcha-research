d4410a9df3d37ae0f8347dd70ad01218d72d71f7
dropout <dropout@fbi.ac>
2024-09-05 14:49:47 +1200

    Update decrpyt-encrypt.py

--- scripts/decrpyt-encrypt.py
+++ scripts/decrpyt-encrypt.py
@@ -8,7 +8,7 @@ class hCaptcha:
     def __init__(self, version: str = "6c2596db2ce08d2f8763801d158624c790db3d34b0235bb33999fd85979fac64") -> None:
         self.key_chain = {
             "6c2596db2ce08d2f8763801d158624c790db3d34b0235bb33999fd85979fac64": [57, 193, 86, 78, 153, 197, 48, 29, 30, 110, 66, 69, 142, 163, 29, 31,158, 85, 63, 51, 59, 157, 80, 43,198, 155, 22, 23, 170, 244, 181, 109],
-            "d1285730e6df7d9e9b8c090abc80b36cbbf8722e3655a8d303e084cc7824831a": [208, 37, 218, 11, 6, 85, 42, 133] # I'll let you find these ones. (But there is the start of it as a hint.)
+            "d1285730e6df7d9e9b8c090abc80b36cbbf8722e3655a8d303e084cc7824831a": [208, 37, 218, 11, 6, 85, 42, 133, 191, 44, 194, 245, 8, 152, 47, 32, 135, 246, 54, 205, 137, 131, 85, 50, 158, 226, 135, 163, 17, 60, 22, 0] # other version :)
         }
 
         if version not in self.key_chain:
